e7190a286d54b7fbd7787cf10bc937875a9edfa2
chandrakanthm <chandra619kanth@gmail.com>
2016-02-14 14:18:41 -0700

    working code with text file embedded

--- userspace.c
+++ userspace.c
@@ -8,33 +8,22 @@ int main()
 	// declare return value for system call
 	int ret = 0;
 	// declare buffer to hold result of system call
-	char * buffer;
+	char * buff;
 	// set the length of the buffer
-	int n = 9000;
+	int n = 5000;
 
 	// allocate memory for buffer to hold result
-	if( ( buffer = (char *) malloc(n*sizeof(char)+1) ) == NULL ) 
+	if( ( buff = (char *) malloc(n*sizeof(char)+1) ) == NULL ) 
 		perror( "Error allocating buffer\n" );
-
-	// print the size of the buffer
-	// printf("buffer size = %d\n", n);
-
 	// perform system call, storing return value in ret
 	// result of system call will be stored in buffer
-	ret = syscall(__NR_my_syscall, 0, n, buffer);
+	ret = syscall(__NR_my_syscall, 0, n, buff);
 
 	// print the results of the system call - contents of buffer
-	// printf("buffer = %s\n", buffer);
-	printf("  PID TTY          TIME CMD\n");
-	printf("%s", buffer);
-
-	// print the return value from system call
-	// this should have the number of bytes not copied to userspace
-	// printf("ret = %d\n", ret);
+	printf("PID TTY TIME CMD\n");
+	printf("%s", buff);
 
 	// de-allocate the memory allocated for the buffer
-	free(buffer);
-
-	// return 0 upon success
+	free(buff);
 	return 0;
 }
